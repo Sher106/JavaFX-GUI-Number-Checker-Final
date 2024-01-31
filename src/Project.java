7d0226c7aecf62459689255540ebd32ea936c7ab
Sherica Hall <halls16@montclair.edu>
2024-01-31 00:26:20 -0500

    Additional comments added

--- src/Project.java
+++ src/Project.java
@@ -1,3 +1,11 @@
+//**************************************************************************
+
+// Project.java     Author: Hall/ Sherica
+//
+//Demonstrates Number Checker, Image Display and Scene change in GUI
+//*************************************************************************
+
+//Imports for JavaFX functioning in the code
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Rectangle2D;
@@ -13,21 +21,25 @@ import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.scene.text.Text;
 
-class Project2 extends Application {
+//Creating a Class to build the code in, so that everything happens within the Project
+public class Project extends Application
+{
+
+    //Declaring individual components so that they can be used throughout the code
     protected Text text = new Text(30, 20, "Counter");
     public RadioButton leftButton, rightButton;
-    public Image img, img2;
+    public Image img;
     public VBox finalStack;
     public TextField num;
     public ImageView imageView;
-    public String url, url2;
-    public Button submit;
-    public Stage primaryStage;
     public int number;
 
+//Start is a class in the Application
     @Override
-    public void start(Stage primaryStage) {
-        // Creating RadioButtons
+    public void start(Stage primaryStage)
+    {
+
+        // Creating RadioButtons, its specifications and action response
         HBox paneForRadioButtons = new HBox(20);
 
         num = new TextField();
@@ -35,15 +47,17 @@ class Project2 extends Application {
 
         ToggleGroup group = new ToggleGroup();
 
-        leftButton = new RadioButton("Left");
+        leftButton = new RadioButton("Left");  // Labeling Button
         leftButton.setToggleGroup(group);
+        leftButton.setTranslateY(10);  //Positioning Button in the Y plane
 
-        rightButton = new RadioButton("Right");
+        rightButton = new RadioButton("Right");  //Labeling Button
         rightButton.setToggleGroup(group);
+        rightButton.setTranslateY(10);  //Positioning Button in the Y plane
 
 
         paneForRadioButtons.getChildren().addAll(leftButton, rightButton);
-        paneForRadioButtons.setAlignment(Pos.CENTER);
+        paneForRadioButtons.setAlignment(Pos.CENTER); //Setting general position of RadioButton
 
 
         // Creating Buttons
@@ -52,30 +66,32 @@ class Project2 extends Application {
         submit.setOnAction(this::processButtonPress);
         paneForButtons.getChildren().addAll(submit);
         paneForButtons.setAlignment(Pos.CENTER);
+        submit.setTranslateY(20);
 
-        // Input Image
+        // Creating Image specifications
+        imageView = new ImageView(img);  //Creating a stage for image to be seen
 
-        imageView = new ImageView(img);
-
-        imageView.setViewport(new Rectangle2D(60, 60, 300, 300));
-
+        //Positioning image
+        imageView.setViewport(new Rectangle2D(60, 60, 400, 500));
+        imageView.setTranslateY(40);
+        imageView.setTranslateX(40);
 
         finalStack = new VBox(text, num, paneForRadioButtons, paneForButtons, imageView);
         finalStack.setStyle("-fx-background-color: lightgreen");
 
         // Create a scene and place it in the stage
-        Scene scene = new Scene(finalStack, 450, 500);
+        Scene scene = new Scene(finalStack, 500, 600);
         primaryStage.setTitle("Project");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
+//Creating instructions for button so image changes and background changes
     private void processButtonPress(ActionEvent event) {
         if (leftButton.isSelected()) {
-            finalStack.setStyle("-fx-background-color: lightpink");
+            finalStack.setStyle("-fx-background-color: lightpink"); //Setting a style for left button
         } else if (rightButton.isSelected()) {
-            finalStack.setStyle("-fx-background-color: purple");
+            finalStack.setStyle("-fx-background-color: purple");  //Setting a style for right button
         } else {
             finalStack.setStyle("-fx-background-color: lightgreen");
         }
@@ -89,10 +105,7 @@ class Project2 extends Application {
         imageView.setImage(img);
     }
 
-
-    //Method for Submit Button
-
-
+    //Where execution of method created starts
     public static void main(String[] args) {
         launch(args);
     }
